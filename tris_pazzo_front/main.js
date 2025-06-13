8e043a7dd8a64fbc4c6c0b0b1818353fd60ab3d2
AlessandroLisanti <alessandro.lisanti07@gmail.com>
2025-06-13 09:46:25 +0200

    .

--- tris_pazzo_front/main.js
+++ tris_pazzo_front/main.js
@@ -1,3 +1,5 @@
+const socket = new WebSocket("wss://uc4cu1bz76.execute-api.eu-north-1.amazonaws.com");
+
 function getNickname() {
   const nick = document.getElementById("nickname").value.trim();
   if (!nick) {
@@ -13,6 +15,31 @@ function Send(){
   if(val==null) console.log("errore")
   else {
     location.href="FindLobby.html"
+    socket.send(JSON.stringify({
+    action: "$connect",
+    nickname: getNickname()
+    }));
   }
 
 }
+
+socket.onopen = () => {
+  console.log("✅ Connesso al WebSocket AWS");
+};
+
+// Quando ricevi un messaggio dal server
+socket.onmessage = (event) => {
+  console.log("📨 Messaggio ricevuto:", event.data);
+};
+
+// Gestione errori
+socket.onerror = (err) => {
+  console.error("❌ Errore WebSocket:", err);
+};
+
+// Connessione chiusa
+socket.onclose = () => {
+  console.log("🔌 Connessione chiusa");
+};
+
+
