7f124f3b93d60853be6accfad2f4a9f39669da9d
SimoneDursi <simone.dursi07@gmail.com>
2025-06-13 09:59:30 +0200

    Update Lambda1.py

--- Lambda1.py
+++ Lambda1.py
@@ -1,17 +1,24 @@
 import json
 import boto3
 
+# Connessione a DynamoDB
 dynamodb = boto3.resource('dynamodb', region_name='eu-north-1')
-table = dynamodb.Table('nome_da_inserire_dynamodb')
+table_users = dynamodb.Table('tris_pazzo_utenti')
 
+# Funzione per aggiungere utente a DynamoDB con il modello richiesto
+def add_user(connection_id, nickname,idstanza):
+    table_users.put_item(Item={'connectionId': connection_id, 'nickname': nickname,'id_stanza':idstanza})
+    return {"status": "Utente aggiunto con successo"}
+
+# Lambda Handler
 def lambda_handler(event, context):
-    #websocket
-    print(event)
+    print("Evento ricevuto:", event)
+
+    # Recupera l'ID della connessione WebSocket
     connection_id = event['requestContext']['connectionId']
+    body = json.loads(event.get("body", "{}"))
+    nickname = body['data'['nickname']]
+    idstanza = body['data'['id_stanza']]
+    add_user(connection_id, nickname,idstanza)
 
-    #quando un utente si connette mettiamo il suo id come pk, poi il nickname e infine lo stato per vedere se è pronto: 1 si 0 no
-    table.put_item(Item={'pk':connection_id,'nickname':"gino",'stato':0})
-
-    table.delete_item(Item={'pk':connection_id,'nickname':"gino",'stato':1})
-    
-    return {'statusCode': 200}
+    return {"statusCode": 200}
